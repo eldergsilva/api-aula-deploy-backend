c739b0325b57ed6dcdb4de4ea1aef116c84738bc
unknown <eldergsilva@gmail.com>
2023-10-19 11:46:14 -0300

    Ajustado o path do .env

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-require('dotenv').config()
-const express = require('express')
-const knex =require('./conexao.js')
-const app = express()
+require('dotenv').config({ path: './src/.env' });
+const express = require('express');
+const knex = require('./conexao.js');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', async (req, res) => {
-	try {
-	 const carros = await knex('carros')
-	 return res.json(carros)
-	} catch (error) {
-		console.log(error)
-		return res.status(500).json({mensagem:'Erro interno do Servidor'})	
-	}
+  try {
+    const carros = await knex('carros');
+    return res.json(carros);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensagem: 'Erro interno do Servidor' });
+  }
+});
 
-})
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log(`Servidor em pé na porta ${port}`)
-})
-
-
+  console.log(`Servidor em pé na porta ${port}`);
+});
